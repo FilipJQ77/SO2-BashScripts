7d361b0158fd2e1fa7bf665829d55c0c4f0ca2dd
Filip Przygoński <50529336+FilipJQ77@users.noreply.github.com>
2021-06-23 21:36:57 +0200

    Add files via upload

---
+++ lab3/zad8.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 3 – semestr letni 2020/2021
+#
+# Celem zajęć jest pogłębienie wiedzy na temat struktury systemu plików,
+# poruszania się po katalogach i kontroli uprawnień w skryptach powłoki.
+#
+# Przygotowane rozwiązania nie muszą być całkowicie uniwersalne. Zakładamy,
+# że ogólna struktura katalogów się nie zmienia (nie będzie już więcej/mniej
+# poziomów podkatalogów), jednakże same nazwy i zawartości plików (o ile
+# nie są podane wprost w treści zadań) mogą być dowolne i ulegać zmianie,
+# a przygotowane rozwiązania nadal powinny działać.
+#
+# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
+# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
+# pliki pomocnicze należy usunąć.
+#
+
+#
+# Zadanie 8.
+# Odszukać w katalogu `ccc` wiszące dowiązania i podjąć próbę ich naprawy
+# – zakładamy, że wskazywane przez nie nazwy plików są poprawne, ale doszło
+# do jakiegoś zamieszania w strukturze katalogów. Należy odszukać pasujących
+# plików w katalogach `aaa` i `bbb`, a jeśli odpowiednie pliki istnieją
+# to wyświetlić nazwę dowiązania i po dwukropku proponowaną poprawną ścieżkę
+# (na przykład: bravo:../aaa/bravo).
+#
+cd ccc
+
+for file in *; do
+    if [[ -L "${file}" && ! -e "${file}" ]] ; then
+        fixed="$(find ../aaa/ ../bbb/ -name "$(basename "$(readlink "${file}")")" | head -n1)"
+        if [[ ! -z "${fixed}" ]]; then
+            echo "${file}:${fixed}"
+        fi        
+        # realpath --relative-to=ccc $(find aaa/ -name 'alfa' | head -n1)
+        # "$(basename "$(readlink "${file}")")"
+    fi
+done
